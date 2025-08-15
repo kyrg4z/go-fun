6f8333a09877b30474e23eda57512254c2f3ed8b
kyrg4z <strong.willed343@gmail.com>
2025-08-14 23:08:35 -0400

    performance speed solved, gitignore fixed

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"net"
 	"sort"
+	"sync"
+	"time"
 )
 
-func worker(ports, results chan int) {
+func worker(ports, results chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for p := range ports {
 		address := fmt.Sprintf("scanme.nmap.org:%d", p)
-		conn, err := net.Dial("tcp", address)
+		conn, err := net.DialTimeout("tcp", address, 1*time.Second)
 		if err != nil {
 			results <- 0
 			continue
@@ -20,25 +23,42 @@ func worker(ports, results chan int) {
 }
 
 func main() {
-	ports := make(chan int, 100)
-	results := make(chan int)
+	const numWorkers = 1000
+	const maxPorts = 1024
+	
+	ports := make(chan int, numWorkers)
+	results := make(chan int, maxPorts)
 	var openports []int
-	for i := 0; i < cap(ports); i++ {
-		go worker(ports, results)
+	var wg sync.WaitGroup
+	
+	// Start workers
+	wg.Add(numWorkers)
+	for range numWorkers {
+		go worker(ports, results, &wg)
 	}
+	
+	// Send ports to scan
 	go func() {
-		for i := 1; i <= 1024; i++ {
+		defer close(ports)
+		for i := 1; i <= maxPorts; i++ {
 			ports <- i
 		}
 	}()
-	for range 1024 {
-		port := <-results
+	
+	// Close results channel when all workers are done
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+	
+	// Collect results
+	for port := range results {
 		if port != 0 {
 			openports = append(openports, port)
 		}
 	}
-	close(ports)
-	close(results)
+	
+	// Sort open ports & print
 	sort.Ints(openports)
 	for _, port := range openports {
 		fmt.Printf("%d open\n", port)
